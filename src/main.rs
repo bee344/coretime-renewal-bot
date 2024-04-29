6ae2c43e77f25a10415740599def6b65df046680
bee344 <alberto.penayo@parity.io>
2024-04-29 08:31:28 -0300

    updated metadata

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,11 @@ use subxt_signer::sr25519::dev::{self};
 
 #[subxt::subxt(
     runtime_metadata_path = "./metadata.scale")]
-pub mod polkadot {}
+pub mod rococo {}
 
-async fn check_balance(address: AccountId32, api: OnlineClient::<PolkadotConfig>) -> Option<polkadot::runtime_types::pallet_balances::types::AccountData<::core::primitive::u128>> {
+async fn check_balance(address: AccountId32, api: OnlineClient::<PolkadotConfig>) -> Option<rococo::runtime_types::pallet_balances::types::AccountData<::core::primitive::u128>> {
 
-    let balance_query = polkadot::storage().balances().account(address);
+    let balance_query = rococo::storage().balances().account(address);
     let balance = api.storage().at_latest().await.unwrap().fetch(&balance_query).await.unwrap();
 
     return balance;
@@ -22,7 +22,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     const CORETIME_URI: &str = "ws://127.0.0.1:9920";
 
     let coretime_api = OnlineClient::<PolkadotConfig>::from_url(CORETIME_URI).await.unwrap();
-    let constant_query = polkadot::constants().balances().existential_deposit();
+    let constant_query = rococo::constants().balances().existential_deposit();
     let existential_deposit = coretime_api.constants().at(&constant_query)?;
 
     let mut blocks_sub = coretime_api.blocks().subscribe_finalized().await?;
@@ -34,7 +34,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
         let events = block.events().await?;
 
-        for event in events.find::<polkadot::broker::events::Renewable>() {
+        for event in events.find::<rococo::broker::events::Renewable>() {
             let free_balance = check_balance(alice.public_key.into().clone(), coretime_api.clone()).await.unwrap().free;
 
             let evt = event?;
@@ -43,7 +43,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             let core = evt.core;
 
             if core == core_to_renew && free_balance >= ( price + existential_deposit ) {
-                let renewal_tx = polkadot::tx().broker().renew(core);
+                let renewal_tx = rococo::tx().broker().renew(core);
 
                 coretime_api.tx().sign_and_submit_then_watch_default(&renewal_tx, &alice)
                 .await?
