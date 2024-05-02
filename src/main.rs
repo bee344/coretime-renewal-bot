ceb080700916e9ae98311e1e04f1f0fc189a4dde
bee344 <alberto.penayo@parity.io>
2024-05-02 07:51:01 -0300

    updated args

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,9 @@ use std::env;
     runtime_metadata_path = "./metadata.scale")]
 pub mod rococo {}
 
-type AccountData = rococo::runtime_types::pallet_balances::types::AccountData<::core::primitive::u128>;
-type AccountInfo = rococo::runtime_types::frame_system::AccountInfo<::core::primitive::u32, AccountData>;
-type StatusRecord = rococo::runtime_types::pallet_broker::types::StatusRecord;
+type AccountData = rococo::runtime_types::pallet_balances::types::AccountData<u128>;
+type AccountInfo = rococo::runtime_types::frame_system::AccountInfo<u32, AccountData>;
+type SaleInfoRecord = rococo::runtime_types::pallet_broker::types::SaleInfoRecord<u128, u32>;
 
 async fn check_balance(address: AccountId32, api: OnlineClient::<PolkadotConfig>) -> Option<AccountInfo> {
 
@@ -16,7 +16,12 @@ async fn check_balance(address: AccountId32, api: OnlineClient::<PolkadotConfig>
     let balance = api.storage().at_latest().await.unwrap().fetch(&balance_query).await.unwrap();
 
     return balance;
+}
 
+async fn get_sale_info(api: OnlineClient::<PolkadotConfig>) -> Option<SaleInfoRecord> {
+    let info = api.storage().at_latest().await.unwrap().fetch(&rococo::storage().broker().sale_info()).await.unwrap();
+
+    return info;
 }
 
 #[tokio::main]
@@ -26,20 +31,21 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let address: AccountId32 = alice.public_key().into();
 
-    const uri: &str = &args[1];
+    let uri: &str = &args[1];
 
-    const core_to_renew: &str = &args[2]
+    let string_to_renew: &str = &args[2];
+    let core_to_renew: u16 = string_to_renew.parse::<u16>().expect("Invalid core ID.");
+
+    let mut renewable = false;
+    let mut price: u128 = 0;
+    let mut core: u16 = 0;
+    let mut begin: u32 = 0;
 
     let coretime_api = OnlineClient::<PolkadotConfig>::from_url(uri).await.unwrap();
     let constant_query = rococo::constants().balances().existential_deposit();
     let existential_deposit = coretime_api.constants().at(&constant_query)?;
 
     let mut blocks_sub = coretime_api.blocks().subscribe_finalized().await?;
-
-    let mut renewable = false;
-    let mut price: u128 = 0;
-    let mut core: u16 = 0;
-    let mut begin: u32 = 0;
 
     while let Some(block) = blocks_sub.next().await {
         let block = block?;
@@ -54,17 +60,17 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             core = evt.core;
 
             if core == core_to_renew {
-                
                 price = evt.price;
                 begin = evt.begin;
                 renewable = true;
+                println!("Core is renewable from region {begin} at the price of {price}.");
             }
         }
 
         if renewable {
-            let sale_info = coretime_api.storage().at_latest().await.unwrap().fetch(&rococo::storage().broker().sale_info()).await.unwrap();
+            let sale_info = get_sale_info(coretime_api.clone()).await.unwrap();
 
-            if sale_info.unwrap().region_begin == begin {
+            if sale_info.region_begin == begin {
                 let free_balance = check_balance(address.clone(), coretime_api.clone()).await.unwrap().data.free;
 
                 if free_balance >= ( price + existential_deposit ) {
@@ -75,15 +81,15 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                     .wait_for_finalized_success()
                     .await?;
 
-                    println!("core renewed: {core} on bloc #{block_number}");
+                    println!("Renewed core #{core} on block #{block_number}.");
 
                     renewable = false;
 
+                } else {
+                    println!("Core not renewed due to lack of funds.");
                 }
             }
-            
         }
-    
     }
 
     Ok(())
