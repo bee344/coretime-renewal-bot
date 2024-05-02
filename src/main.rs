25fb190b2a3eed1a11430bea1b783c88b14e8295
bee344 <alberto.penayo@parity.io>
2024-05-02 08:42:54 -0300

    updated checks for renewal window

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ pub mod rococo {}
 type AccountData = rococo::runtime_types::pallet_balances::types::AccountData<u128>;
 type AccountInfo = rococo::runtime_types::frame_system::AccountInfo<u32, AccountData>;
 type SaleInfoRecord = rococo::runtime_types::pallet_broker::types::SaleInfoRecord<u128, u32>;
+type ConfigRecord = rococo::runtime_types::pallet_broker::types::ConfigRecord<u32, u32>;
 
 async fn check_balance(address: AccountId32, api: OnlineClient::<PolkadotConfig>) -> Option<AccountInfo> {
 
@@ -24,6 +25,12 @@ async fn get_sale_info(api: OnlineClient::<PolkadotConfig>) -> Option<SaleInfoRe
     return info;
 }
 
+async fn get_broker_config(api:OnlineClient::<PolkadotConfig>) -> Option<ConfigRecord> {
+    let broker_config = api.storage().at_latest().await.unwrap().fetch(&rococo::storage().broker().configuration()).await.unwrap();
+
+    return broker_config;
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
@@ -36,13 +43,15 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let string_to_renew: &str = &args[2];
     let core_to_renew: u16 = string_to_renew.parse::<u16>().expect("Invalid core ID.");
 
+
     let mut renewable = false;
     let mut price: u128 = 0;
     let mut core: u16 = 0;
     let mut begin: u32 = 0;
-
+    let mut renewal_start: u32 = 0;
     let coretime_api = OnlineClient::<PolkadotConfig>::from_url(uri).await.unwrap();
     let constant_query = rococo::constants().balances().existential_deposit();
+    let interlude_length = get_broker_config(coretime_api.clone()).await.unwrap().interlude_length;
     let existential_deposit = coretime_api.constants().at(&constant_query)?;
 
     let mut blocks_sub = coretime_api.blocks().subscribe_finalized().await?;
@@ -68,27 +77,35 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
 
         if renewable {
+            let free_balance = check_balance(address.clone(), coretime_api.clone()).await.unwrap().data.free;
+
+            let enough_funds = free_balance >= ( price + existential_deposit );
+            
+            if !enough_funds {
+                println!("Funds not enough to renew the core, please add more funds to your account.");
+                continue
+            }
+
             let sale_info = get_sale_info(coretime_api.clone()).await.unwrap();
 
             if sale_info.region_begin == begin {
-                let free_balance = check_balance(address.clone(), coretime_api.clone()).await.unwrap().data.free;
-
-                if free_balance >= ( price + existential_deposit ) {
-                    let renewal_tx = rococo::tx().broker().renew(core);
-
-                    coretime_api.tx().sign_and_submit_then_watch_default(&renewal_tx, &alice)
-                    .await?
-                    .wait_for_finalized_success()
-                    .await?;
-
-                    println!("Renewed core #{core} on block #{block_number}.");
-
-                    renewable = false;
-
-                } else {
-                    println!("Core not renewed due to lack of funds.");
-                }
+                renewal_start = block_number;
             }
+
+            if sale_info.region_begin >= begin && ( renewal_start + interlude_length ) <= block_number {
+                println!("In renewal window, attempting to renew core #{core_to_renew}.");
+                let renewal_tx = rococo::tx().broker().renew(core);
+
+                coretime_api.tx().sign_and_submit_then_watch_default(&renewal_tx, &alice)
+                .await?
+                .wait_for_finalized_success()
+                .await?;
+
+                println!("Renewed core #{core} on block #{block_number}.");
+
+                renewable = false;
+            }
+
         }
     }
 
